6d92a1914e7882001ebff74f2d9e4c3d1cbf54d7
aylr <taylormiller@gmail.com>
2015-07-02 22:05:15 -0600

    yo

--- garden_controller_v0.1.c
+++ garden_controller_v0.1.c
@@ -1,6 +1,3 @@
-// This #include statement was automatically added by the Spark IDE.
-#include "TimeAlarms/TimeAlarms.h"
-
 // ************************* DEFINITIONS *************************
 #define ONE_DAY_MILLIS (24 * 60 * 60 * 1000)
 #define ZONE0 0
@@ -10,15 +7,53 @@
 
 // ************************* VARIABLES *************************
 
+bool disable_watering = false;
 unsigned long last_cloud_time_sync = millis();
 byte zone_state_array[4] = {0,0,0,0};
 byte zones[4] = {ZONE0, ZONE1, ZONE2, ZONE3};
 byte test_timer = 20;	// seconds for "relay test heartbeat timer"
+unsigned long default_watering_interval = ONE_DAY_MILLIS;	// seconds until next watering event
+unsigned long default_watering_duration = 10;				// seconds to water
+unsigned long api_interval = 10*1000;
 
 
+// ************************* TIMER CLASS *************************
+
+class Timer{
+		// class member variables initialized at startup
+		unsigned long timer_last_millis;							// stores the last event
+		unsigned long timer_interval_millis;						// stores the interval called for
+		
+	public:
+		// constructor - creates a timer and initializes variables
+		Timer(unsigned long interval){
+			timer_interval_millis = interval;
+		}
+		bool checkTimer(){
+			unsigned long timer_current_millis = millis();			// get current 'time'
+			
+			if((timer_current_millis - timer_last_millis) >= timer_interval_millis){
+				timer_last_millis = timer_current_millis;
+				return true;										// it's time!
+			}else{
+				return false;										// it's not time.
+			}
+		}
+		unsigned long interval(){
+			return timer_interval_millis;
+		}
+		unsigned long last_ran() {
+			return timer_last_millis;
+		}
+		unsigned long next_run() {
+			return timer_last_millis + timer_interval_millis;
+		}
+};
 
 
 // ************************* SETUP *************************
+Timer zone_0_timer(api_interval);
+Timer zone_1_timer(20*1000);
 
 void setup() {
 	// set time zone to MST (daylight -6, std -7) on each boot
@@ -33,17 +68,13 @@ void setup() {
 
 	publish_event("GardenPhoton Booted", NULL);
 
-	Alarm.alarmRepeat(6,00,0, Repeats);  // 8:30am every day
-	// Alarm.alarmRepeat(17,45,10,EveningAlarm);  // 5:45pm every day 
-	// Alarm.alarmRepeat(dowSaturday,8,30,30,WeeklyAlarm);  // 8:30:30 every Saturday 
-
-    // Alarm.timerRepeat(test_timer, Repeats);     // timer for every 15 seconds    
-
 	Spark.function("test", API_test);
+	Spark.function("handler", handler);
 }
 
 int API_test(String command) {
 	toggle_zone_state(ZONE0);
+	update_interval()
 	publish_event("api", NULL);
 }
 
@@ -68,12 +99,63 @@ bool toggle_zone_state(byte zone) {
 
 // ************************* MAIN LOOP *************************
 void loop() {
-	Alarm.delay(0); // required for all alarms (the point where it can check)
+	run_every_loop();
+	
+
+
+
+}
+
+// ************************* INTERVAL FUNCTIONS *************************
+void run_every_loop() {
 	if (check_sync_timer()) { 
-		sync_time_with_cloud();
+			sync_time_with_cloud();
 	}
+	if ( zone_0_timer.checkTimer() ) {			// check timer on zone 0
+		toggle_zone_state(0);
+	}
+	if ( zone_1_timer.checkTimer() ) {			// check timer on zone 1
+		toggle_zone_state(1);
+	}
+}
 
 
+// ************************* API FUNCTIONS *************************
+
+int handler(String inputString){
+	byte tempReturn;
+	String trimmedInputString = inputString.substring(1);	// trim off first charcter
+
+	switch (inputString.substring(0,1).toInt()){			// switch based on first character
+		case 0:												// 0 = all off
+			zones_all_off();
+			publish_event("mode", "off");
+			tempReturn = 0;
+			break;
+		case 1:												// 1 = disable automatic watering
+			disable_watering = true;
+			publish_event("mode", "disable watering");
+			tempReturn = 1;
+			break;
+		case 2:												// change zone state
+			// ***** make a string to array function that you can pass the dirty string in and
+			// get an array (or pointer to an array) containing all the clean data
+			// then pass that clean data to the given function.
+			change_zone_valve(trimmedInputString);
+			publish_event("mode", "gradient");
+			tempReturn = 2;
+			break;
+		default:
+			tempReturn = -1;
+			break;
+	}
+	updateAPIStripColors32();	// set the spark variable string with strip colors
+	mode = tempReturn;	// update the current spark variable mode
+	return tempReturn;	// return with the mode number
+}
+
+int string_to_array_ptr(){
+	return 0;
 }
 
 
